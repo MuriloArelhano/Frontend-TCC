f067536cb3fbd84a9eedad7d1f7d019ec50d4e9e
Lucas Avanzi <lucasavanzi97@gmail.com>
2021-05-15 13:04:20 -0400

    Corrigindo validação do status da resposta do cadastro

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useHistory } from 'react-router-dom';
 import { Navbar, Footer, PasswordStrength } from "../../components";
 // notification
@@ -56,7 +56,7 @@ const Signup = () => {
       } else {
         const response = await AuthAPI.signUp(authData);
 
-        if (response === 200) {
+        if (response.status === 200) {
           setAuthData({ name: '', email: '', password: '', confirmPassword: '' });
 
           setTimeout(() => {
