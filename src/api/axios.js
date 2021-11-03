3999f973a520da6f8a04eae7b9b71e085c892fdf
Murilo Esteca Arelhano <51941771+MuriloArelhano@users.noreply.github.com>
2021-11-03 15:51:41 -0400

    https

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const axiosConfig = axios.create({
-  baseURL: `http://${process.env.REACT_APP_API_URL}`
+  baseURL: `https://${process.env.REACT_APP_API_URL}`
   
 });
 
